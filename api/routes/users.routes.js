e6e6257781950407be8770f63384ccf5bc101547
Oleh Khashchevskyi <o.khashchevskyi@gmail.com>
2022-08-10 14:14:54 +0300

    #3

--- api/routes/users.routes.js
+++ api/routes/users.routes.js
@@ -4,7 +4,7 @@ const router = new Router();
 
 router.get('/', async (req, res) => {
   try {
-    const users = await User.find();
+    const users = await User.find({...req.query});
     return res.json(users);
   } catch (e) {
     console.log(e);
