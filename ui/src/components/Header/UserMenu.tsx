1444d916a363147e5c3706ea64cc79a0d6237061
Oleh Khashchevskyi <o.khashchevskyi@gmail.com>
2022-08-12 22:45:25 +0300

    #8 udapted user menu

--- ui/src/components/Header/UserMenu.tsx
+++ ui/src/components/Header/UserMenu.tsx
@@ -47,6 +47,27 @@ const UserMenu:React.FC = () => {
         id="account-menu"
         open={openMenu}
         onClose={handleToggleMenu}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            minWidth: '160px',
+            overflow: 'visible',
+            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            mt: 1.5,
+            '&:before': {
+              content: '""',
+              display: 'block',
+              position: 'absolute',
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: 'background.paper',
+              transform: 'translateY(-50%) rotate(45deg)',
+              zIndex: 0,
+            },
+          },
+        }}
       >
         <MenuItem onClick={handleToggleMenu} component={NavLink} to="admin/my-profile">
           <ListItemIcon>
