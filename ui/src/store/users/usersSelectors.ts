ee708dc54cc5a0beefc50072322b7079c4fa5ec8
Pavlo Khashchevskiy <PikiMapo92@gmail.com>
2022-08-14 22:49:06 +0300

    #3 added pagination

--- ui/src/store/users/usersSelectors.ts
+++ ui/src/store/users/usersSelectors.ts
@@ -2,3 +2,5 @@ import { RootState } from "..";
 
 export const selectCurrentUser = (state: RootState) => state.users.currentUser;
 export const selectUsers = (state: RootState) => state.users.users;
+export const selectTotal = (state: RootState) => state.users.total;
+export const selectParams = (state: RootState) => state.users.params;
