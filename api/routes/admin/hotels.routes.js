c6ab7b4a76e0f8f5fdca3f43e7e38ddc45779995
Oleh Khashchevskyi <o.khashchevskyi@gmail.com>
2023-01-29 23:46:53 +0200

    updated hotel routes

--- api/routes/admin/hotels.routes.js
+++ api/routes/admin/hotels.routes.js
@@ -10,8 +10,9 @@ const authMiddleware = require('../../middlewares/auth.middleware');
 
 router.get('/', authMiddleware, async (req, res) => {
   try {
-    const { limit, page } = req.query;
-    const params = { ...req.query };
+    const { limit, page, search, ...nextParams } = req.query;
+    const regex = new RegExp(search, 'gi');
+    const params = { ...nextParams, name: { '$regex': regex } };
     const total = await Hotel.find(params).count();
     const hotels = await Hotel.find(params).skip((page-1)*limit).limit(limit)
       .populate({ path: 'hotelType', populate: { path: 'image' } })
